81f9d72c9875a0a7fd502582d0c0b89ef8c7d4ff
realariful <real_ariful@hotmail.com>
2021-12-28 09:51:23 +0600

    [FIX] Files

---
+++ react-director-admin-template-master/docs/assets/precache-manifest.22ce1a2870c6b561cd2d4375183acda6.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dbaf9da64b14d9b230fd1775e14622",
+    "url": "/assets/../index.html"
+  },
+  {
+    "url": "/assets/app.238e97d6a42b2a3119d9.js"
+  },
+  {
+    "revision": "901b9bef3c6a8f8b111c647e995a9d0b",
+    "url": "/assets/modernizr-bundle.js"
+  },
+  {
+    "url": "/assets/styles.238e97d6a42b2a3119d9.js"
+  },
+  {
+    "url": "/assets/vendors.238e97d6a42b2a3119d9.js"
+  }
+]);
---
+++ react-director-admin-template-master/src/front/components/tabPanel/index.js
@@ -0,0 +1,9 @@
+// @flow
+
+export { default as TabPanel } from './TabPanel/TabPanel';
+export { default as TabPanelHeader } from './tabPanelHeader/TabPanelHeader';
+export { default as TabPanelBody } from './tabPanelBody/TabPanelBody';
+export {
+  default as TabPanelBodyContent,
+} from './TabPanelBodyContent/tabPanelBodyContent';
+export { default as TabPanelDemo } from './TabPanelDemo';
